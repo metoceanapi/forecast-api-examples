7267bd3a04a5d3af1f7ec5aa75864b3bb0286f0c
Oscar Key <oscar.c.key@gmail.com>
2023-01-24 22:37:31 +1300

    add wind vector example and refactor code for it

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ import { argv } from 'node:process'
 import { inspect } from 'node:util'
 import { makeOptions, fetcher } from './index.js'
 import { pointTime } from './point-time.js'
+import { pointTimeWindVectors } from './point-time-wind-vectors.js'
+
+function pretty(o) {
+  return inspect(o, {showHidden: false, depth: null, colors: true})
+}
 
 function main() {
   let key = argv.pop()
@@ -9,10 +14,15 @@ function main() {
   let options = makeOptions(target.data, key)
 
   fetcher(target.url, options, function(data) {
-    console.log('API response JSON:', inspect(data, {showHidden: false, depth: null, colors: true}))
-    if(target.cb !== undefined) {
-      target.cb(data)
+    console.log('API response JSON:', pretty(data))
+    if(target.cb === undefined) {
+      return
     }
+    let processed = target.cb(data)
+    if (processed === undefined) {
+      return
+    }
+    console.log('Processed:', pretty(processed))
   })
 }
 
