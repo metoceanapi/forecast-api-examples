38dbe7e905ea204af0b1c4f3f5af85371f5114f2
Oscar Key <oscar.c.key@gmail.com>
2023-01-25 21:42:09 +1300

    add base64 js example

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { makeOptions, fetcher } from './index.js'
 import { pointTime } from './point-time.js'
 import { pointTimeWindVectors } from './point-time-wind-vectors.js'
 import { pointTimeMasks } from './point-time-masks.js'
+import { pointTimeBase64 } from './point-time-base64.js'
 
 function pretty(o) {
   return inspect(o, {showHidden: false, depth: null, colors: true})
