dc85b221cf49945d7bad58deb2c4786323be6ff5
Oscar Key <oscar.c.key@gmail.com>
2023-02-24 13:16:48 +1300

    golang: add wind examples

--- golang/main.go
+++ golang/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"reflect"
 	"sort"
@@ -60,6 +61,55 @@ func unpackBase64(r PointResponse) (any, error) {
 	return b64.response.UnpackData()
 }
 
+func directionFromComponents(u, v float32) float32 {
+	dividend := math.Atan2(-float64(u), -float64(v)) * (180 / math.Pi)
+	return float32(math.Mod(dividend+360, 360))
+}
+
+func magnitudeFromComponents(u, v float32) float32 {
+	return float32(math.Sqrt(math.Pow(float64(u), 2) + math.Pow(float64(v), 2)))
+}
+
+func windFromComponents(r PointResponse) (any, error) {
+	wrapper, ok := r.(*PointResponseFloat)
+	if !ok {
+		return nil, fmt.Errorf("Couldn't cast argument to a float response")
+	}
+	regular := wrapper.response
+	north, found := regular.Variables["wind.speed.northward.at-10m"]
+	if !found {
+		return nil, fmt.Errorf("Northward wind component not found")
+	}
+	east, found := regular.Variables["wind.speed.eastward.at-10m"]
+	if !found {
+		return nil, fmt.Errorf("Eastward wind component not found")
+	}
+	n := len(east.Data)
+	if len(north.Data) != n {
+		return nil, fmt.Errorf("Northward and eastward wind components have different lengths")
+	}
+
+	direction := make([]float32, n)
+	magnitude := make([]float32, n)
+	noData := make(Uint8SliceWrapper, n)
+	for index := range east.Data {
+		// the data are already initialised to 0 in the slice, just like Golang's JSON deserialisation if it encounters a null in an array of numbers
+		if east.NoData[index] != 0 {
+			noData[index] = east.NoData[index]
+			continue
+		}
+		if north.NoData[index] != 0 {
+			noData[index] = north.NoData[index]
+			continue
+		}
+		direction[index] = directionFromComponents(east.Data[index], north.Data[index])
+		magnitude[index] = magnitudeFromComponents(east.Data[index], north.Data[index])
+	}
+	regular.Variables["wind.direction.at-10m"] = VariableFloat{Data: direction, NoData: noData, Variable: Variable{Dimensions: east.Dimensions, Units: "meterPreSecond", SIUnits: "m.s^{-1}", StandardName: "wind_from_direction_at_10m_above_ground_level"}}
+	regular.Variables["wind.speed.at-10m"] = VariableFloat{Data: magnitude, NoData: noData, Variable: Variable{Dimensions: east.Dimensions, Units: "degree", SIUnits: "degree", StandardName: "wind_speed_at_10m_above_ground_level"}}
+	return regular, nil
+}
+
 func ptr[V any](v V) *V {
 	return &v
 }
@@ -81,8 +131,10 @@ func mapKeys(m any) []string {
 var (
 	now      = time.Now()
 	examples = map[string]Example{
-		"pointTime":       {request: pointTime, response: &PointResponseFloat{}},
-		"pointTimeBase64": {request: pointTimeBase64, response: &PointResponseBase64{}, process: unpackBase64},
+		"pointTime":            {request: pointTime, response: &PointResponseFloat{}},
+		"pointTimeBase64":      {request: pointTimeBase64, response: &PointResponseBase64{}, process: unpackBase64},
+		"pointTimeWind":        {request: pointTimeWind, response: &PointResponseFloat{}},
+		"pointTimeWindVectors": {request: pointTimeWindVectors, response: &PointResponseFloat{}, process: windFromComponents},
 	}
 
 	pointTime = PointRequest{
@@ -104,6 +156,26 @@ var (
 		},
 		Variables: []string{"wave.height"},
 		Format:    ptr("base64"),
+	}
+
+	pointTimeWind = PointRequest{
+		Points: []Point{{Longitude: 174.7842, Latitude: -37.7935}},
+		Time: TimeSequence{
+			From:     &now,
+			Interval: ptr(Duration{time.Hour * 3}),
+			Repeat:   ptr(uint32(2)),
+		},
+		Variables: []string{"wind.speed.at-10m", "wind.direction.at-10m"},
+	}
+
+	pointTimeWindVectors = PointRequest{
+		Points: []Point{{Longitude: 174.7842, Latitude: -37.7935}},
+		Time: TimeSequence{
+			From:     &now,
+			Interval: ptr(Duration{time.Hour * 3}),
+			Repeat:   ptr(uint32(2)),
+		},
+		Variables: []string{"wind.speed.northward.at-10m", "wind.speed.eastward.at-10m"},
 	}
 
 	apiKey  = flag.String("apikey", "", "API key to use for requests.")
