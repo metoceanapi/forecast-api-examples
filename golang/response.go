4c49879674d216a9b7b6a69db16b1d52bd9f6926
Oscar Key <oscar.c.key@gmail.com>
2023-02-24 13:12:40 +1300

    tidy base64 unpacking, add safe example, and format output nicely

--- golang/response.go
+++ golang/response.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+type Uint8SliceWrapper []uint8 // golang's JSON package serialises []byte/[]uint8 as base64 by default, but we want human-readable output
+
 type VariableFloat struct {
 	Variable
-	Data   []float32 `json:"data"` //  N.B. during decoding, nulls are skipped
-	NoData []uint8   `json:"noData"`
+	Data   []float32         `json:"data"`   //  N.B. during decoding, nulls are skipped
+	NoData Uint8SliceWrapper `json:"noData"` //
 }
 
 type PointDataResponseFloat struct {
@@ -35,4 +42,8 @@ func (r *PointResponseFloat) Response() any {
 
 func (r *PointResponseFloat) ResponsePtr() any {
 	return &r.response
+}
+
+func (s Uint8SliceWrapper) MarshalJSON() ([]byte, error) {
+	return []byte(strings.Join(strings.Fields(fmt.Sprint(s)), ",")), nil
 }
--- golang/responseb64.go
+++ golang/responseb64.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"unsafe"
 )
 
 type VariableBase64 struct {
 	Variable
-	Data []byte `json:"data"` // golang treats []byte as base64-encoded by default
+	Data []byte `json:"data"` // golang's JSON package serialises []byte as base64 by default, and it will also deserialise base64 strings into []byte
 }
 
 type PointDataResponseBase64 struct {
@@ -40,21 +43,38 @@ func (r *PointResponseBase64) ResponsePtr() any {
 	return &r.response
 }
 
-func (v VariableBase64) UnpackData(maskLut map[uint32]uint8) VariableFloat {
-	regular := VariableFloat{Variable: v.Variable}
+func UnpackSlice(data []byte, nFloats uintptr, maskLut map[uint32]uint8) ([]float32, error) {
+	floats := make([]float32, nFloats)
+	return floats, binary.Read(bytes.NewReader(data), binary.LittleEndian, &floats)
+}
+
+func UnpackSliceUnsafeLE(data []byte, nFloats uintptr, maskLut map[uint32]uint8) ([]float32, error) {
+	// use this if you know you are running on a little-endian platform
+	return unsafe.Slice((*float32)(unsafe.Pointer(&data[0])), nFloats), nil
+}
+
+func (v VariableBase64) UnpackData(maskLut map[uint32]uint8) (VariableFloat, error) {
+	regular := VariableFloat{Variable: v.Variable, Data: make([]float32, 100)}
+
 	sizeOfF32 := unsafe.Sizeof(float32(0))
-	n := uintptr(len(v.Data)) / sizeOfF32
-	regular.Data = unsafe.Slice((*float32)(unsafe.Pointer(&v.Data[0])), n)
-	regular.NoData = make([]uint8, n)
+	nBytes := uintptr(len(v.Data))
+	n := nBytes / sizeOfF32
+	if n*sizeOfF32 != nBytes { // this should never happen - if it does, please report a bug
+		return regular, fmt.Errorf("Cannot unpack data: %v is not a multiple of %v bytes", nBytes, sizeOfF32)
+	}
+
+	regular.NoData = make(Uint8SliceWrapper, n)
 	for index := range regular.NoData {
 		offset := uintptr(index) * sizeOfF32
-		u32 := *(*uint32)(unsafe.Pointer(&v.Data[offset]))
+		u32 := binary.LittleEndian.Uint32(v.Data[offset:])
 		if mask := maskLut[u32]; mask != 0 {
 			regular.NoData[index] = mask
 		}
 	}
 
-	return regular
+	var err error
+	regular.Data, err = UnpackSlice(v.Data, n, maskLut)
+	return regular, err
 }
 
 func (r PointDataResponseBase64) UnpackData() (PointDataResponseFloat, error) {
@@ -73,8 +93,11 @@ func (r PointDataResponseBase64) UnpackData() (PointDataResponseFloat, error) {
 		NoDataReasons: reasons,
 		Variables:     make(map[string]VariableFloat, len(r.Variables)),
 	}
+	var err error
 	for name, v := range r.Variables {
-		regular.Variables[name] = v.UnpackData(maskLut)
+		if regular.Variables[name], err = v.UnpackData(maskLut); err != nil {
+			return regular, fmt.Errorf("Failed to unpack %v: %v", name, err)
+		}
 	}
 	return regular, nil
 }
